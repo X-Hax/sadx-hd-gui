7b1d0e6f34155517aa3de1019653b74d7c8ad233
PkR <PiKeyAr@users.noreply.github.com>
2024-04-16 20:22:21 +0900

    Add trampoline macros

--- HD_GUI/Common.h
+++ HD_GUI/Common.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#define TARGET_DYNAMIC(name) ((decltype(name##_r)*)name##_t->Target())
+#define TARGET_STATIC(name) ((decltype(name##_r)*)name##_t.Target())
+
 #define ReplacePVMX(a) helperFunctions.ReplaceFile("system\\" a ".PVM", "system\\" a "_HD.PVM")
 
 void LoadSubtitleFont();
